1ecd7d44909e43b113401152b7ec17f4b1bbba6c
ana004 <paulaana08002019@gmail.com>
2022-05-31 14:42:10 -0300

    operacao somar

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var operacao = args[0];
+var n1 = Convert.ToDouble(args[1]);
+var n2 = Convert.ToDouble(args[2]);
+
+
+if (operacao == "Somar")
+{
+    Console.WriteLine(n1+n2);
+}
